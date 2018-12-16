801dd6e6e77971996abb8a78841fbcf1f90f2962
Ali Alavi <ali.alavi201@gmail.com>
2018-12-15 16:37:07 -0800

    Detecting Zambonie

--- kernel/train.c
+++ kernel/train.c
@@ -5,7 +5,7 @@ This program solves the model train track challenge
 */
 
 char res_buf[3];
-
+int zambonie_found = 0;
 #include <kernel.h>
 
 void send_com_message(int input_buf_len, char * cmd)
@@ -40,11 +40,39 @@ void init_track_switches()
     send_com_message(0, switches[i]);
 }
 
+void check_zambonie(int window_id)
+{
+  int i = 0;
+
+  while(i < 40)
+  {
+    send_com_message(0,"R\015");
+    send_com_message(3,"C10\015"); // segment in between both starting points for Zambonie
+
+    if((char)res_buf[1] == '1')
+    {
+      zambonie_found = 1;
+      return;
+    }
+
+    sleep(5);
+    i++;
+  }
+
+  return;
+}
+
 void train_process(PROCESS self, PARAM param)
 {
   int window_id = wm_create(6, 4, 15, 15);
   wm_print(window_id, "Setting up outer switches\n");
   init_track_switches();
+  check_zambonie(window_id);
+
+  if(zambonie_found)
+    wm_print(window_id, "Zambonie Found");
+  else
+    wm_print(window_id, "Zambonie Not Found");
 }
 
 void init_train()
